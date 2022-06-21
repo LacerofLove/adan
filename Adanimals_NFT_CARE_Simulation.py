52cf65f076857e8ef0547acbc7311d0623e31639
LacerofLove <107947847+LacerofLove@users.noreply.github.com>
2022-06-21 21:59:08 +0200

    Update

--- Adanimals_NFT_CARE_Simulation.py
+++ Adanimals_NFT_CARE_Simulation.py
@@ -23,24 +23,26 @@ if 'count' not in st.session_state:
     #st.session_state.last_updated = datetime.time(0,0)
 
 def update_counter():
-    st.session_state.CARE_increment += st.session_state.CARE_increment
-    st.session_state.month_increment += st.session_state.month_increment
+    st.session_state.CARE_increment = st.session_state.CARE_increment
+    #st.session_state.month_increment = st.session_state.month_increment
 
     #st.session_state.last_updated = st.session_state.update_time
 
 df, monthly_CARE_arr_24_8000, NFT_monthly_arr = load_data()
 df_T = df.T
 
+
+default_val_CARE = int(NFT_monthly_arr[0])
+
 with st.form(key='my_form'):
     #st.time_input(label='Enter the time', value=datetime.datetime.now().time(), key='update_time')
-    st.number_input('Month of staking (1-24)', value=0, step=1, key='month_increment')
-    st.number_input('CARE per month for NFT staking', value=int(NFT_monthly_arr[st.session_state.month_increment]), step=1, key='CARE_increment')
+    #st.number_input('Month of staking (1-24)', value=0, step=1, key='month_increment')
+    st.number_input('CARE per month for NFT staking', value=default_val_CARE, step=1, key='CARE_increment')
     submit = st.form_submit_button(label='Update', on_click=update_counter)
 
-st.write('Current Count = ', st.session_state.count)
+#st.write('Current Month = ', st.session_state.month_increment)
+st.write('Total $CARE tokens emitted per month = ', st.session_state.CARE_increment)
 #st.write('Last Updated = ', st.session_state.last_updated)
-
-
 
 
 st.title('Adanimals NFT Simulator')
@@ -48,7 +50,7 @@ st.title('Adanimals NFT Simulator')
 
 
 
-df_T['monthly $CARE'] = (df_T['Weight (%)']*st.session_state.CARE_increment).astype(int)
+df_T['monthly $CARE'] = (df_T['Weight (%)']*st.session_state.CARE_increment/100).astype(int)
 
 
 df_T
